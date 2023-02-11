9ab62871a96fbdc804847a86da56b044e928675f
FelipeReis1 <feliperg15@yahoo.com.br>
2023-02-10 23:38:57 -0300

    feat: customers router added

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import gamesRouter from "./gamesRouter.js";
-
+import customersRouter from "./customersRouter.js";
 const route = Router();
 
 route.use(gamesRouter);
+route.use(customersRouter);
 
 export default route;
