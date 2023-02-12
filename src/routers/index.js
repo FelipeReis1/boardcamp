a344cd5c4e9402eee37b5895f96f91d9975ae8d9
FelipeReis1 <feliperg15@yahoo.com.br>
2023-02-12 19:09:46 -0300

    feat: added rentals router

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import gamesRouter from "./gamesRouter.js";
 import customersRouter from "./customersRouter.js";
+import rentalsRouter from "./rentalsRouter.js";
 const route = Router();
 
 route.use(gamesRouter);
 route.use(customersRouter);
+route.use(rentalsRouter);
 
 export default route;
